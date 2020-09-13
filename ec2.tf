60eb5141a181f5f7a0453d3d4c1a035e14f0710e
Mohd Aslam <45025384+mohd-aslam@users.noreply.github.com>
2020-09-13 09:29:01 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "webserver01" {
   key_name               = "terraform"
   subnet_id              = "subnet-0fd0d7bdabc405307"
   vpc_security_group_ids = ["sg-0774b1360cffb5e3f"]
-  user_data              = file("install_httpd.sh")
   tags = {
     Name = "webserver01"
   }
